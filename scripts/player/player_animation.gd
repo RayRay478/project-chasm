778b83968c6ba6e4e5102e61ce784ae0c87341bd
hypersonic250 <gbaltar725@gmail.com>
2026-01-28 00:44:06 -0500

    TUNING EXPORTS + ANIMATION FIXES + PHYSICS POLISHING. JUST A FEW RECODING STUFF BASICALLY
    LOTS OF NEW TUNING EXPORTS AND ANIMATION/PHYSICS POLISHING !!!!!

--- scripts/player/player_animation.gd
+++ scripts/player/player_animation.gd
@@ -1,17 +1,18 @@
 extends Node3D
+
 class_name Body
 
-
 const LERP_VELOCITY: float = 0.15
-
-
-@onready var player = get_node_or_null("../..") # change path to your actual player path
+# @onready var player = get_node("../../../../../..")
+# changes path to your actual player path (changed all the @onready $Something with exports; 
+#less hardcoding paths, faster to debug + safer - h)
+@export var player: CharacterBody3D
+@export var character: CharacterBody3D
+@export var animation_player: AnimationPlayer
 
 @export var target_name: String = "Player"
 
 @export_category("Objects")
-@export var _character: CharacterBody3D = null
-@export var animation_player: AnimationPlayer = null
 
 func apply_rotation(_velocity: Vector3) -> void:
 	var new_rotation_y = lerp_angle(rotation.y, atan2(-_velocity.x, -_velocity.z), LERP_VELOCITY)
@@ -19,18 +20,51 @@ func apply_rotation(_velocity: Vector3) -> void:
 
 
 func animate(_velocity: Vector3) -> void:
-
-	if not _character.is_on_floor():
-		if _velocity.y < 0:
-			animation_player.play("Air")
-		else:
-			var current_anim = animation_player.current_animation
-			if current_anim != "Jump" and current_anim != "Jump":
-				animation_player.play("Jump")
+	if character == null or animation_player == null:
 		return
-		
-	else: 
-		animation_player.play("Idle ")
+
+	var want: String
+
+	# AIR
+	if not character.is_on_floor():
+		want = "Air" if _velocity.y < 0.0 else "Jump"
+	else:
+		# GROUND SPEED (horizontal only)
+		var speed := character.velocity.slide(character.up_direction).length()
+
+		if speed < 0.1:
+			want = "Idle "      # (NOTE: there's a space after Idle in the AnimPlayer - h)
+		elif speed < 18.0:
+			want = "Walk"
+		else:
+			want = "Run"
+
+	# Play only if changed or finished
+	if animation_player.current_animation != want or not animation_player.is_playing():
+		animation_player.play(want)
+
+
+	# OLD GROUNDED ANIM CODE (just for safekeeping?) - h
+	# if player == null or animation_player == null:
+	#	return
+
+	# if not player.is_on_floor():
+	#	if _velocity.y < 0:
+	#		animation_player.play("Air")
+	#	else:
+	#		animation_player.play("Jump")
+	#	return
+
+	# animation_player.play("Idle ")
+
+
+	# OLD GROUNDED ANIM CODE (just for safekeeping?) - h
+	# if player.accel_speed >= 0.5:
+	#	animation_player.play("Run")
+#	else:
+#		animation_player.play("Idle ")
+
+
 
 
 func play_jump_animation(jump_type: String = "Jump") -> void:
--- scripts/player/player_main.gd
+++ scripts/player/player_main.gd
@@ -13,13 +13,26 @@ const debug_enabled: bool = true
 @export var jump_impulse := 12.0
 @export var turnspeed_curve: Curve
 @export var _gravity := -30.0
+@export var slope_gravity := 1.2   # the strength of slopes' gravity
+@export var ground_friction := 12.0
+@export var max_downhill_accel := 30.0
+@export var max_uphill_accel := 20.0
+@export var uphill_drag_build := 0.8     # builds while climbing
+@export var uphill_drag_decay := 1.6     # recovers when not climbing
+@export var uphill_drag_max_loss := 0.5  # extra target-speed loss at full drag
+@export var uphill_min_speed_ratio := 0.25  # doesn't drop below 25% of move_speed from drag
 
 
+
+
+
+
+
+
+var accel_speed: float = 0.0
 var last_player_input_dir: Vector3 = Vector3.ZERO
+var uphill_drag: float = 0.0 # 0..1
 
-
-
-@onready var camera = $Camera/CameraPivot/SpringArm3D/Camera3D
 
 
 @export_group("Camera")
@@ -35,7 +48,7 @@ var gsp: float = 0.0:
 
 var abs_gsp: float = 0.0
 
-var _camera_input_direction := Vector2.ZERO
+var camera_input_direction := Vector2.ZERO
 var _last_movement_direction := Vector3.BACK
 #var _gravity_normal: Vector3 = Vector3.UP 
 
@@ -46,12 +59,14 @@ var slope_normal: Vector3 = get_floor_normal()
 	#	slope_mag_dot = slope_normal.dot(_gravity_normal)
 
 
-@onready var _camera: Camera3D = $Camera/CameraPivot/SpringArm3D/Camera3D
-@onready var _body: Node3D = $Player/Sonic
+@export var camera: Camera3D
+@export var body: Body
+# @export var body: Node3D 
+# (Body instead of Node3D might help avoid errors for things like body.animate() - h)
+@export var debug_label: Label
 
-@onready var debug_label: Label = $Debug/Label
 
-# DEBUG STUFF
+# ^^ DEBUG STUFF ^^ - (added var camera here just for better organization - h)
 
 func readable_vector(input:Vector3) -> String:
 	return str(input.snappedf(0.01))
@@ -72,20 +87,39 @@ func _enter_tree():
 
 
 func _ready():
-	camera.current = is_multiplayer_authority()
+	if camera:
+		camera.current = is_multiplayer_authority()
+
 
 # func _unhandled_input(event: InputEvent) -> void:
 
 
 func _physics_process(delta: float) -> void:
+	#if camera == null or body == null or debug_label == null:
+	#	return
+	
+	
+	if camera == null or body == null or debug_label == null:
+		print("Missing refs:",
+			" camera=", camera,
+			" body=", body,
+			" debug_label=", debug_label)
+		return
+
+	# rest of movement...
+
 
 	debug_label.text = ""
+	camera_input_direction = Vector2.ZERO
 
-	_camera_input_direction = Vector2.ZERO
+	
 
 	#var _axis: Vector3 = _gravity_normal.cross(slope_normal).normalized()
 	
-	var calcForward = _camera.global_position.direction_to(global_position).slide(up_direction)
+	#var calcForward = _camera.global_position.direction_to(global_position).slide(up_direction)
+	var calcForward = camera.global_position.direction_to(global_position).slide(up_direction)
+
+	
 	
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var input_3: Vector3 = Vector3(
@@ -103,25 +137,80 @@ func _physics_process(delta: float) -> void:
 	velocity.y = 0.0
 	
 	var previous_velocity = velocity.dot(up_direction)
-	var acclspeed: float = velocity.length() / (move_speed)
-	
+	# accel_speed = velocity.length() / move_speed (old code for safekeeping - h)
+	var horiz_speed := velocity.slide(up_direction).length()
+	accel_speed = clampf(horiz_speed / move_speed, 0.0, 1.0)
+	# (clampf is "clamp float." takes a number and forces it to stay WITHIN the min and max ranges - h)
 
 	
 	if direction:
+		var target_speed := move_speed
+
+		if is_on_floor():
+			var n := get_floor_normal()
+			var g_vec := up_direction * _gravity
+			var downhill := g_vec.slide(n)
+			var downhill_len := downhill.length()
+
+			if downhill_len > 0.001:
+				var downhill_dir := downhill / downhill_len
+
+		# 0..1 steepness (flat -> steep)
+				var steep := clampf(downhill_len / absf(_gravity), 0.0, 1.0)
+
+		# how "uphill" the input direction is (0 downhill/flat, 1 fully uphill)
+				var uphillness := clampf((-direction).dot(downhill_dir), 0.0, 1.0)
+
+		# tune this (0.3..0.8). Higher = more speed loss uphill.
+				var uphill_loss := 0.6
+
+		# reduce target speed uphill based on steepness
+				target_speed *= (1.0 - uphill_loss * uphillness * steep)
+				
+							# Build/decay uphill drag over time (stateful slowdown)
+				var climbing := uphillness * steep  # 0..1
+
+			# only build drag if we're actually moving
+				var moving := velocity.slide(up_direction).length() > 0.5
+				if not moving:
+					climbing = 0.0
+
+					uphill_drag = clampf(
+							uphill_drag + climbing * uphill_drag_build * delta
+							- (1.0 - climbing) * uphill_drag_decay * delta,
+							0.0, 1.0
+			)
+
+			# Apply extra loss that increases the longer you climb
+					target_speed *= (1.0 - uphill_drag_max_loss * uphill_drag)
+
+			# Prevent target from collapsing to near-zero
+			target_speed = maxf(target_speed, move_speed * uphill_min_speed_ratio)
 
 
 		if velocity.slide(up_direction).length() < move_speed:
 
 			# main acceleration
 		#	velocity = velocity.slide(up_direction).move_toward(direction * move_speed, acceleration * delta)+previous_velocity*up_direction
-			velocity = velocity.slide(up_direction).move_toward(direction * move_speed, acceleration_curve.sample(move_speed))
+			var horiz := velocity.slide(up_direction)
+			var speed := horiz.length()
+			var t := clampf(speed / move_speed, 0.0, 1.0)
+
+			var curve_val := acceleration_curve.sample(t)
+			var min_curve := 0.20 # tune: 0.10–0.35
+			var accel_step := acceleration * maxf(curve_val, min_curve) * delta
+
+			#swapped from move_speed to target_speed for uphill math
+			velocity = horiz.move_toward(direction * target_speed, accel_step)
 			add_debug_info("FUCK IM NOT TURNING")
 
 		else:
-			# turning code
-			velocity = velocity.slide(up_direction).move_toward(direction * move_speed,1)
+			# turning code (old for safekeeping. doing some fuckshit magic rn - h)
+			# velocity = velocity.slide(up_direction).move_toward(direction * move_speed, 1)
+			var turn_step := acceleration * 0.5 * delta  # tune this
+			velocity = velocity.slide(up_direction).move_toward(direction * target_speed, turn_step)
 			rotation_speed = 7
-			add_debug_info("IM TURNING NIGGA")
+			add_debug_info("IM TURNING YOOOOO!!!")
 
 
 	else:
@@ -139,7 +228,7 @@ func _physics_process(delta: float) -> void:
 		_last_movement_direction = direction
 		
 	var target_angle := Vector3.BACK.signed_angle_to(_last_movement_direction, Vector3.UP)
-	_body.global_rotation.y = lerp_angle(_body.rotation.y, target_angle, rotation_speed * delta)
+	body.global_rotation.y = lerp_angle(body.rotation.y, target_angle, rotation_speed * delta)
 	
 	# HYPER THIS IS OLD SLOPE CODE TAKEN FROM ANOTHER ENGINE
 
@@ -158,17 +247,76 @@ func _physics_process(delta: float) -> void:
 		#	add_debug_info("RUNNING DOWNHILL")
 		#	gsp += slope_angle * downhill_factor * delta
 		#elif slope_dir_dot > 0: # Uphill
-		#	add_debug_info("RUNNING UP THAT HILL") #kudos if you pick up the ref :trol:
+		#	add_debug_info("RUNNING UP THAT HILL") (#kudos if you pick up the ref :trol:- r) (i hate it. - h)
 		#	gsp -= slope_angle * uphill_factor * delta
 
-	_body.animate(velocity)
-	move_and_slide()
-	
+# =========================================================================
+	move_and_slide() 
+	# HYPER'S INSANE PHYSICS BULLSHIT HERE WE GO
+	if is_on_floor():
+		var n := get_floor_normal()
+		var horiz := velocity.slide(up_direction)
+
+		var g_vec := up_direction * _gravity          # points down
+		var downhill := g_vec.slide(n)                # gravity along slope plane
+		var downhill_len := downhill.length()
+
+		if downhill_len > 0.001:
+			var downhill_dir := downhill / downhill_len
+			var along := horiz.dot(downhill_dir)
+
+	# Steepness (0 = flat, 1 = very steep)
+			var steep := clampf(downhill_len / absf(_gravity), 0.0, 1.0)
+
+			var downhill_mul := 1.4
+			var uphill_base := 0.6
+			var uphill_extra := 2.0
+
+			var mul := downhill_mul if along > 0.0 else (uphill_base + uphill_extra * steep)
+
+	# downhill_len already represents acceleration along the slope
+			var slope_accel := downhill_len * slope_gravity * mul
+
+	# Cap effect so nothing explodes
+			slope_accel = clampf(slope_accel, -max_uphill_accel, max_downhill_accel)
+
+			horiz += downhill_dir * (slope_accel * delta)
+
+	# prevents sonic from GETTING FLUNG when he so much as GRAZES a slope
+			if along < 0.0:
+				var new_along := horiz.dot(downhill_dir)
+				new_along = maxf(new_along, 0.0)
+				horiz = horiz.slide(downhill_dir) + downhill_dir * new_along
+		
+
+		#var n := get_floor_normal()
+
+		#var horiz := velocity.slide(up_direction)
+		#var speed := horiz.length()
+		#var ground_dir := horiz.normalized() if speed > 0.05 else _last_movement_direction.normalized()
+
+		#var g_vec := up_direction * _gravity
+		#var g_parallel := g_vec.slide(n)
+		#var slope_push := g_parallel.dot(ground_dir)
+
+		#horiz += ground_dir * (slope_push * slope_gravity * delta)
+
+		#if direction.length() < 0.1:
+		#	horiz = horiz.move_toward(Vector3.ZERO, ground_friction * delta)
+
+		#velocity.x = horiz.x
+		#velocity.z = horiz.z
+
+	# updates accel+speed AFTER being on a slope
+	#horiz_speed = velocity.slide(up_direction).length()
+	#accel_speed = clampf(horiz_speed / move_speed, 0.0, 1.0)
+
+	body.animate(velocity)
 
 	add_debug_info("Input Vector: " + readable_vector(input_3))
 	add_debug_info("Driection: " + readable_vector(direction))
 	add_debug_info("Velocity: " + readable_vector(velocity))
-	add_debug_info("Speed Up?: " + readable_float(acclspeed))
+	add_debug_info("Speed Up?: " + readable_float(accel_speed))
 	
 	add_debug_info("Target Angle: " + readable_float(target_angle))
 	#add_debug_info("Ground Angle " + readable_float(rad_to_deg(acos(slope_mag_dot))))
